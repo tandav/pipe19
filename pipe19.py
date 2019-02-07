8a810118564b4ab83d918ec3904f7a7d0dfe7559
Alexander Rodionov <alx1dr@gmail.com>
2019-02-08 00:24:05 +0300

    more readable control flow (return early)
    Signed-off-by: Alexander Rodionov <alx1dr@gmail.com>

--- pipe19.py
+++ pipe19.py
@@ -1,13 +1,16 @@
 def handle_task(task):
-    '''maybe there's more readable control flow'''
-    if not task.isdone():
-        if task.deps:
-            if type(task.deps) is list:
-                for dep in task.deps:
-                    handle_task(dep)
-            else: # when deps is single task
-                handle_task(task.deps)
-                assert task.deps.isdone()
+    if task.isdone():
+        return
 
-        task.run()
-        assert task.isdone(), 'task is not done after run() for some reason'
+    if task.deps:
+        if type(task.deps) is list:
+            deps = task.deps
+        else: # when deps is single task
+            deps = [task.deps]
+
+        for dep in deps:
+            handle_task(dep)
+            assert dep.isdone()
+
+    task.run()
+    assert task.isdone(), 'task is not done after run() for some reason'
